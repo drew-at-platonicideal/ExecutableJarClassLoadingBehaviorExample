1ec1a3e5a38ebe4df243e564d0366ce20fff5da7
drew-at-titus <57359899+drew-at-titus@users.noreply.github.com>
2020-01-26 09:53:48 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class App {
+    
+    public static void main(String a[]) throws ClassNotFoundException, IOException {
+        URL externalResource = new File("C:\\Users\\Admin\\PlatonicIdeal\\workspace\\demo\\src\\main\\resources\\external_resource-0.0.1-SNAPSHOT.jar").toURI().toURL();
+        
+        try(URLClassLoader urlClassLoader = new URLClassLoader(new URL[] {externalResource})) {
+            Class<?> klazz = urlClassLoader.loadClass("com.example.external_resource.FooController");
+            System.out.println(klazz);
+        }
+        
+    }
+
+}
